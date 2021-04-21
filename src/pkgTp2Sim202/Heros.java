116220366d2f51e30ab7f2fe113b81827d6a3004
Alyah8509 <82900605+Alyah8509@users.noreply.github.com>
2021-04-21 19:13:53 -0400

    finiNiveau

--- src/pkgTp2Sim202/Heros.java
+++ src/pkgTp2Sim202/Heros.java
@@ -1,4 +1,31 @@
 package pkgTp2Sim202;
 
 public class Heros {
+    int vie=6;
+    int force=1;
+    int cristaux=0;
+    public int getVie (){
+        return vie;
+    }
+    public int getForce(){
+        return force;
+    }
+    public int getCristaux(){
+        return cristaux;
+    }
+    public void additionnerVie(int combien){
+        vie+=combien;
+        if (vie>6){
+            vie=6;
+        }
+    }
+    public boolean diminuerVie (int combien){
+        vie-=combien;
+        if (vie<=0){
+            return false;
+        }else {return true;}
+    }
+    public void additionnerCristaux (){
+        cristaux++;
+    }
 }
--- src/pkgTp2Sim202/Partie.java
+++ src/pkgTp2Sim202/Partie.java
@@ -1,62 +1,23 @@
 package pkgTp2Sim202;
 
 
+import java.util.Scanner;
 
 public class Partie {
     boolean statut=false;
     Vie vieActuelle=Vie.VIVANT;
     char [][] map;
     int niveau=1;
+    Heros Adlez=new Heros();
 
     public void jouer (){
+        Scanner sc=new Scanner(System.in);
+        String actions;
         while (!statut){
             Niveau floor1=new Niveau();
-            map=floor1.lire(niveau);
-            for (int i=0; i< map.length; i++){
-                for (int y=0; y< map[0].length; y++){
-                    System.out.print(map[i][y]);
-                }System.out.println();
-            }
-            niveau++;
-            Niveau floor2=new Niveau();
-            map=floor2.lire(niveau);
-            for (int i=0; i< map.length; i++){
-                for (int y=0; y< map[0].length; y++){
-                    System.out.print(map[i][y]);
-                }System.out.println();
-            }
-            niveau++;
-            Niveau floor3=new Niveau();
-            map=floor3.lire(niveau);
-            for (int i=0; i< map.length; i++){
-                for (int y=0; y< map[0].length; y++){
-                    System.out.print(map[i][y]);
-                }System.out.println();
-            }
-            niveau++;
-            Niveau floor4=new Niveau();
-            map=floor4.lire(niveau);
-            for (int i=0; i< map.length; i++){
-                for (int y=0; y< map[0].length; y++){
-                    System.out.print(map[i][y]);
-                }System.out.println();
-            }
-            niveau++;
-            Niveau floor5=new Niveau();
-            map=floor5.lire(niveau);
-            for (int i=0; i< map.length; i++){
-                for (int y=0; y< map[0].length; y++){
-                    System.out.print(map[i][y]);
-                }System.out.println();
-            }
-            niveau++;
-            Niveau floor6=new Niveau();
-            map=floor6.lire(niveau);
-            for (int i=0; i< map.length; i++){
-                for (int y=0; y< map[0].length; y++){
-                    System.out.print(map[i][y]);
-                }System.out.println();
-            }
+            afficher(floor1);
+            actions=sc.nextLine();
+
             niveau++;
         statut=true;
         }
@@ -65,5 +26,15 @@ public class Partie {
         MORT,
         VIVANT,
         GAMECLEAR
+    }
+    private void afficher(Niveau actuelle){
+        System.out.println("Vies: "+Adlez.getVie()+"/6        Force: "+Adlez.getForce()+"            Cristaux: "+Adlez.getCristaux());
+        map=actuelle.lire(niveau);
+        for (int i=0; i< map.length; i++){
+            for (int y=0; y< map[0].length; y++){
+                System.out.print(map[i][y]);
+            }System.out.println();
+        }
+        System.out.println("Que voulez-vous faire: ");
     }
 }
