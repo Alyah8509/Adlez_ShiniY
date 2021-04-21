0b73fd4f18c56c455f5920f6242004ce61b57689
Alyah8509 <82900605+Alyah8509@users.noreply.github.com>
2021-04-21 10:00:51 -0400

    change1

--- src/pkgTp2Sim202/Main.java
+++ src/pkgTp2Sim202/Main.java
@@ -3,10 +3,8 @@ package pkgTp2Sim202;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Allo");
-        System.out.printf("est-ce que tu vois?");
-
-
+        Partie jeu=new Partie();
+            jeu.jouer();
 
 
 
--- src/pkgTp2Sim202/Partie.java
+++ src/pkgTp2Sim202/Partie.java
@@ -1,4 +1,18 @@
 package pkgTp2Sim202;
 
 public class Partie {
+    boolean statut=false;
+    Vie vieActuelle=Vie.VIVANT;
+
+    public void jouer (){
+        while (!statut){
+
+        statut=true;
+        }
+    }
+    public enum Vie {
+        MORT,
+        VIVANT,
+        GAMECLEAR
+    }
 }
