7a165cc97719035fb1aec7c92686648d01f10f92
Spinhugo <82841139+Spinhugo@users.noreply.github.com>
2021-05-03 21:34:15 -0400

    main

--- src/pkgTp2Sim202/Monstre.java
+++ src/pkgTp2Sim202/Monstre.java
@@ -5,52 +5,25 @@ public class Monstre extends Personnage {
     private int hp;
     private int x;
     private int y;
-
-    /**
-     * Constructeur de monstre, décide du hp et force au début
-     * @param attaque
-     * @param hp
-     */
     public Monstre(int attaque, int hp) {
         this.attaque = attaque;
         this.hp = hp;
     }
-
-    /**
-     * retourne x
-     * @return
-     */
     public int getX (){return x;}
-
-    /**
-     * retourne la vie
-     * @return
-     */
     public int getVie (){return hp;}
-
-    /**
-     * retourner Y
-     * @return
-     */
     public int getY (){return y;}
-
-    /**
-     * Il regarde les coordonnées de Adlez et détermine s'il faut se déplacer ou attaquer
-     * @param Adlez
-     * @param map
-     */
     public void verifier (Heros Adlez, Tuile [][]map){
-        if (Adlez.getX()-x==-1||Adlez.getX()-x==0||Adlez.getX()-x==1){//c'est des maths
+        if (Adlez.getX()-x==-1||Adlez.getX()-x==0||Adlez.getX()-x==1){
             if (Adlez.getY()-y==-1||Adlez.getY()-y==0||Adlez.getY()-y==1){
-                attaquerHeros(Adlez);//voir la méthode attaquerHeros
+                attaquerHeros(Adlez);
             }
-            else {//s'ils ont le même x mais pas le même y
-                if (Adlez.getY()-y<=-1){//s'il est au moins 2 cases en haut
-                    if (map[y-1][x].getVide()){//vérifie la Tuile (mur/trésor ou non)
-                        partir(map,x,y);//part de cette coordonnée
+            else {
+                if (Adlez.getY()-y<=-1){
+                    if (map[y-1][x].getVide()){
+                        partir(map,x,y);
                         y-=1;
-                        marcher(map,x,y);}//et il marche vers le haut
-                }else if (Adlez.getY()-y>=1){//même chose, si Adlez est en bad
+                        marcher(map,x,y);}
+                }else if (Adlez.getY()-y>=1){
                     if (map[y+1][x].getVide()){
                         partir(map,x,y);
                         y+=1;
@@ -58,12 +31,12 @@ public class Monstre extends Personnage {
                 }
             }
         }
-        else {//si Adlez est loin
-        if (Adlez.getX()-x<=-1){//analyse les coordonnées x
-            if (map[y][x-1].getVide()){//si Adlez est à gauche
+        else {
+        if (Adlez.getX()-x<=-1){
+            if (map[y][x-1].getVide()){
                 partir(map,x,y);
-                x-=1;//avance vers la gauche: change x dans cette classe
-                marcher(map,x,y);//et mise à jour dans la carte 2d de Tuile
+                x-=1;
+                marcher(map,x,y);
             }
         }else if (Adlez.getX()-x>=1){
             if (map[y][x+1].getVide()){
@@ -72,7 +45,7 @@ public class Monstre extends Personnage {
                 marcher(map,x,y);
             }
         }
-        if (Adlez.getY()-y<=-1){//même chose avec Y
+        if (Adlez.getY()-y<=-1){
             if (map[y-1][x].getVide()){
                 partir(map,x,y);
             y-=1;
@@ -86,51 +59,62 @@ public class Monstre extends Personnage {
         }
     }
 
-    /**
-     * Déplacement vers les coordonnées décidés
-     * @param map
-     * @param x
-     * @param y
-     */
+    public void marcher(Tuile[][] map, int x, int y) {
+        map[y][x].setMonstre();
+        map[y][x].setPersonnage(true);
+        this.x=x;this.y=y;
 
-    public void marcher(Tuile[][] map, int x, int y) {//simplement placer @ sur une tuile
-        map[y][x].setMonstre();//voir setMonstre dans Plancher
-        map[y][x].setPersonnage(true);//cette Tuile a un personnage présentemment (pour ne pas disparaître après que
-        //Adlez marche dessus (Voir Plancher pour plus d'explication) )
-        this.x=x;this.y=y;//change x et y
+        //for (int i = 0; i < map.length; i++) {
+           //for (int j = 0; j < map.length; j++) {
+               //faire la condition d'avancement
+                
+        /*attaquerHeros(map,Adlez,coordonner[0],coordonner[1],yAdlez,xAdlez);
+                if (yAdlez - coordonner[1]<= -1 && map[coordonner[1]-1][coordonner[0]]!=mur){
+                    coordonner[1]-=1;
+                }
+                if (yAdlez - coordonner[1] >=1&& map[coordonner[1]+1][coordonner[0]]!=mur){
+                    coordonner[1]+=1;
+                }
+                if (xAdlez-coordonner[0]<=-1&& map[coordonner[1]][coordonner[0]-1]!=mur){
+                    coordonner[0]-=1;
+                }
+                if (xAdlez-coordonner[0]>=1&& map[coordonner[1]][coordonner[0]+1]!=mur){
+                    coordonner[0]+=1;
+                }*/
+            //}
+        //}
     }
 
-    /**
-     * efface @ dans la tuile précédente
-     * @param map
-     * @param x
-     * @param y
-     */
+
+
+
     public void partir(Tuile [][]map, int x, int y){
         map[y][x].setPersonnage(false);
         map[y][x].revenir();
     }
+    public void deplacer (){
 
-    /**
-     * retourne la vie
-     * @return
-     */
+    }
+    public void ff (){
+
+    }
     public int getHp() {
         return hp;
     }
-
-    /**
-     * Diminue la vie de ce héros selon son attaque
-     * @param Adlez
-     */
     private void attaquerHeros(Heros Adlez){
         Adlez.diminuerVie(attaque);
-    }
 
-    /**
-     * Si le monstre est attaqué (selon la force de Adlez)
-     * @param combien
-     */
+            /*if (xAdlez+1 == xMonstre && yAdlez == yMonstre || xAdlez-1 == xMonstre && yMonstre == yAdlez || yAdlez-1== yMonstre && xAdlez == xMonstre || yAdlez+1== yMonstre && xAdlez == xMonstre){
+            //attaquer adlez
+                Adlez.diminuerVie(attaque);
+            }
+            else if (xAdlez + 1 == xMonstre && yAdlez + 1 == yMonstre || xAdlez - 1 == xMonstre && yAdlez + 1 == yMonstre || xAdlez - 1 == xMonstre && yAdlez - 1 == yMonstre || xAdlez + 1 == xMonstre && yAdlez - 1 == yMonstre){
+                Adlez.diminuerVie(attaque);
+            }*/
+
+
+
+    }
     public void diminuerVie (int combien) {
         hp -= combien;
     }
--- src/pkgTp2Sim202/Partie.java
+++ src/pkgTp2Sim202/Partie.java
@@ -8,7 +8,6 @@ public class Partie {
     private Tuile[][] map;
     private boolean quit=false;
     private Heros Adlez = new Heros();
-    private Vie actuelle=Vie.VIVANT;
     private ArrayList<Integer> positionJoueur = new ArrayList<>();
     private ArrayList<Integer> donnesMonstres = new ArrayList<>();
     private ArrayList<Monstre> monstres = new ArrayList<>();
@@ -58,12 +57,11 @@ public class Partie {
             char[] actionEnChar = actions.toCharArray();//transforme le String saisi en char
             for (int i = 0; i < actionEnChar.length; i++) {//et lit les char un par un
                 if (actionEnChar[i] == 'w') {//si c'est w,
-                    //ne peut pas être découpé en méthode vu que X et Y change selon le mouvement.
                     if (map[Adlez.getY() - 1][Adlez.getX()].getVide()) {//vérifie si la case en haut peut être pilé
                         map[Adlez.getY()][Adlez.getX()].revenir();//la case ou Adlez est présentemment devient vide
                         map[Adlez.getY() - 1][Adlez.getX()].setHero();
                         //et le symbole de la case d'en haut est remplacé par &
-                        Adlez.setY(Adlez.getY()-1);//mis à jour des coordonnées de Adlez
+                        Adlez.setY(Adlez.getY()-1);//mis à jour des coordonnées de Adlezz
                     } else {//si getVide retourne false (mur ou trésor)
                         System.out.println("Vous ne pouvez pas marcher par dessus!");
                         break;
@@ -99,7 +97,86 @@ public class Partie {
 
                 }
                 else if (actionEnChar[i]=='c'){
-                    toucher(map,Adlez,messages,niveau);//voir la méthode
+                    ArrayList<Integer> boite=new ArrayList<>();//liste pour stocker les coordonnées temporairement
+                    if (map[Adlez.getY()][Adlez.getX()].getClass()!=Mur.class&&map[Adlez.getY()][Adlez.getX()].getClass()!=Plancher.class){
+                        //du moment que ce n'est pas un mur ou un plancher, on peut interagir avec
+                        if (map[Adlez.getY()][Adlez.getX()].getClass()==Pancarte.class){
+                            messages.afficher();}//si c'est pancarte, un message d'affiche
+                        else if (map[Adlez.getY()][Adlez.getX()].getClass()==Teleporteur.class){//si c'est téléporteur
+                            boite.add(Adlez.getX());boite.add(Adlez.getY());//stocke les données x y de Adlez actuellement
+                            Adlez.setY(map[boite.get(1)][boite.get(0)].getTpy());
+                            //change le Y de Adlez (avec les données du téléporteur ou Adlez était).
+                            //Mais vu que après setY les coordonnées de Adlez change, utiliser getY et getX ne va pas marcher
+                            Adlez.setX(map[boite.get(1)][boite.get(0)].getTpx());
+                            map[Adlez.getY()][Adlez.getX()].setHero();
+                            //aux coordonnées de la classe téléporteur, afficher &
+                            map[boite.get(1)][boite.get(0)].revenir();
+                            //et effacer l'endroit ou Adlez était avant.
+                            boite.clear();//vide la boite pour la suite.
+                        }
+                    }
+                    else if (map[Adlez.getY()][Adlez.getX()+1].getClass()!=Mur.class&&map[Adlez.getY()][Adlez.getX()+1].getClass()!=Plancher.class){
+                        //même chose, à gauche
+                        if (map[Adlez.getY()][Adlez.getX()+1].getClass()==Pancarte.class){
+                            messages.afficher();}
+                        else if (map[Adlez.getY()][Adlez.getX()+1].getClass()==Teleporteur.class){
+                            boite.add(Adlez.getX());boite.add(Adlez.getY());
+                            Adlez.setY(map[boite.get(1)][boite.get(0)+1].getTpy());
+                            Adlez.setX(map[boite.get(1)][boite.get(0)+1].getTpx());
+                            map[Adlez.getY()][Adlez.getX()].setHero();
+                            map[boite.get(1)][boite.get(0)].revenir();
+                            boite.clear();
+
+                        }else if (map[Adlez.getY()][Adlez.getX()+1].getClass()==Tresor.class){
+                            //vu que Adlez ne peut pas marcher sur un trésor, il n'est pas au premier if
+                            map[Adlez.getY()][Adlez.getX()+1].setItem(niveau,Adlez.getX()+1,Adlez.getY());
+                            //lit le fichier pour voir c'est quoi l'item de cette coordonée.
+                            map[Adlez.getY()][Adlez.getX()+1].utiliser(Adlez);
+                            //l'utilise sur Adlez
+                        }
+                    }else if (map[Adlez.getY()][Adlez.getX()-1].getClass()!=Mur.class&&map[Adlez.getY()][Adlez.getX()-1].getClass()!=Plancher.class){
+                        if (map[Adlez.getY()][Adlez.getX()-1].getClass()==Pancarte.class){
+                            messages.afficher();}
+                        else if (map[Adlez.getY()][Adlez.getX()-1].getClass()==Teleporteur.class){
+                            boite.add(Adlez.getX());boite.add(Adlez.getY());
+                            Adlez.setY(map[boite.get(1)][boite.get(0)-1].getTpy());
+                            Adlez.setX(map[boite.get(1)][boite.get(0)-1].getTpx());
+                            map[Adlez.getY()][Adlez.getX()].setHero();
+                            map[boite.get(1)][boite.get(0)].revenir();
+                            boite.clear();
+                        }else if (map[Adlez.getY()][Adlez.getX()-1].getClass()==Tresor.class){
+                            map[Adlez.getY()][Adlez.getX()-1].setItem(niveau,Adlez.getX()-1,Adlez.getY());
+                            map[Adlez.getY()][Adlez.getX()-1].utiliser(Adlez);
+                        }
+                    }else if (map[Adlez.getY()+1][Adlez.getX()].getClass()!=Mur.class&&map[Adlez.getY()+1][Adlez.getX()].getClass()!=Plancher.class){
+                        if (map[Adlez.getY()+1][Adlez.getX()].getClass()==Pancarte.class){
+                            messages.afficher();}
+                        else if (map[Adlez.getY()+1][Adlez.getX()].getClass()==Teleporteur.class){
+                            boite.add(Adlez.getX());boite.add(Adlez.getY());
+                            Adlez.setY(map[boite.get(1)+1][boite.get(0)].getTpy());
+                            Adlez.setX(map[boite.get(1)+1][boite.get(0)].getTpx());
+                            map[Adlez.getY()][Adlez.getX()].setHero();
+                            map[boite.get(1)][boite.get(0)].revenir();
+                            boite.clear();
+                        }else if (map[Adlez.getY()+1][Adlez.getX()].getClass()==Tresor.class){
+                            map[Adlez.getY()+1][Adlez.getX()].setItem(niveau,Adlez.getX(),Adlez.getY()+1);
+                            map[Adlez.getY()+1][Adlez.getX()].utiliser(Adlez);
+                        }
+                    }else if (map[Adlez.getY()-1][Adlez.getX()].getClass()!=Mur.class&&map[Adlez.getY()-1][Adlez.getX()].getClass()!=Plancher.class){
+                        if (map[Adlez.getY()-1][Adlez.getX()].getClass()==Pancarte.class){
+                            messages.afficher();}
+                        else if (map[Adlez.getY()-1][Adlez.getX()].getClass()==Teleporteur.class){
+                            boite.add(Adlez.getX());boite.add(Adlez.getY());
+                            Adlez.setY(map[boite.get(1)-1][boite.get(0)].getTpy());
+                            Adlez.setX(map[boite.get(1)-1][boite.get(0)].getTpx());
+                            map[Adlez.getY()][Adlez.getX()].setHero();
+                            map[boite.get(1)][boite.get(0)].revenir();
+                            boite.clear();
+                        }else if (map[Adlez.getY()-1][Adlez.getX()].getClass()==Tresor.class){
+                            map[Adlez.getY()-1][Adlez.getX()].setItem(niveau,Adlez.getX(),Adlez.getY()-1);
+                            map[Adlez.getY()-1][Adlez.getX()].utiliser(Adlez);
+                        }
+                    }
                     if (niveau==6){//après le 'c', vérifie si le niveau est 6. Si oui, vérifie si Adlez a ouvert un trésor
                         //avec un cristal dedans. Si oui, quit est true et le jeu finit.
                         if (Adlez.getCristaux()==6){
@@ -110,25 +187,17 @@ public class Partie {
 
                 }
                 else if (actionEnChar[i]=='x'){
-                    for (int u=0; u<monstres.size(); u++){//fait un tour dans la liste monstre
+                    for (int u=0; u<monstres.size(); u++){
                         if (Adlez.getX()-monstres.get(u).getX()==-1||Adlez.getX()-monstres.get(u).getX()==0||
-                                Adlez.getX()-monstres.get(u).getX()==1){//si un monstre est au alentours de Adlez
+                                Adlez.getX()-monstres.get(u).getX()==1){
                             if (Adlez.getY()-monstres.get(u).getY()==-1||Adlez.getY()-monstres.get(u).getY()==0||
-                                    Adlez.getY()-monstres.get(u).getY()==1){//en y aussi
-                                Adlez.attaquer(monstres.get(u));//attaque
+                                    Adlez.getY()-monstres.get(u).getY()==1){
+                                Adlez.attaquer(monstres.get(u));
                             }
                         }
-                        if (monstres.get(u).getVie()<=0){//si le monstre est mort
-                            if (Adlez.getX()==monstres.get(u).getX()&&Adlez.getY()==monstres.get(u).getY()){
-                                //situation 1: le monstre est sur Adlez
-                                map[Adlez.getY()][Adlez.getX()].setPersonnage(false);//même chose que partir ici
-                                map[Adlez.getY()][Adlez.getX()].setSymbole("&");
-                                //mais si il part, la méthode devenir va directement transformer symbole en act,
-                                //donc Adlez disparaît. Donc il remplace le symbole par & manuellement.
-                            }
-                            else {monstres.get(u).partir(map,monstres.get(u).getX(),monstres.get(u).getY());}
-                            //si Adlez n'est pas sur le monstre il peut partir directement
-                            monstres.remove(u);//enlevé pour éviter des bugs
+                        if (monstres.get(u).getVie()<=0){
+                            monstres.get(u).partir(map,monstres.get(u).getX(),monstres.get(u).getY());
+                            monstres.remove(u);
                         }
                     }
                 }
@@ -137,142 +206,32 @@ public class Partie {
                     quit=true;
                     break;
                 }
-
                 for (int r=0; r<monstres.size();r++){
-                    //le tour des monstres (voir la classe Monstre)
                     monstres.get(r).verifier(Adlez,map);
                 }
-                if (Adlez.getVie()<=0){//si Adlez meurt
-                    quit=true;//quitter le jeu
-                    actuelle=Vie.MORT;//noter que Adlez est morte
-                    break;
-                }
+
             }
             essais++;
-            if (Adlez.getCristaux()==6){//s'il y a 6 cristaux
-                if (monstres.size()==0){//et plus aucune monstre
-                    actuelle=Vie.GAMECLEAR;//noter que Adlez a gagné
-                    break;
-                }
-            }
-            else if (Adlez.getCristaux()==niveau){
-                //si par contre, Adlez a trouvé le cristal dans le niveau
+            if (Adlez.getCristaux()==niveau){
+                //si par contre, Adlez a trouvé le cristal
                 if (monstres.size()==0){
                     //et qu'il y a aucun monstre, la boucle se brise et Adlez passe au prochain niveau
-                    actuelle=Vie.VIVANT;//noter que Adlez est en vie (passe au niveau suivant)
                     break;
                 }
             }
         }
 
     }
-
-    /**
-     * retourne quitter
-     * @return
-     */
     public boolean getQuit (){
         return quit;
     }
-
-    /**
-     * retourne le statut de Adlez
-     * @return
-     */
-    public Vie getActuelle(){
-        return actuelle;
+    public int getCristaux(){
+        return Adlez.getCristaux();
     }
-
-    /**
-     * Les 3 statuts possibles de Adlez
-     */
     public enum Vie {
         MORT,
         VIVANT,
         GAMECLEAR
-    }
-    public static void toucher (Tuile [][]map, Heros Adlez, Messages messages, int niveau){
-        ArrayList<Integer> boite=new ArrayList<>();//liste pour stocker les coordonnées temporairement
-        if (map[Adlez.getY()][Adlez.getX()].getClass()!=Mur.class&&map[Adlez.getY()][Adlez.getX()].getClass()!=Plancher.class){
-            //du moment que ce n'est pas un mur ou un plancher, on peut interagir avec
-            if (map[Adlez.getY()][Adlez.getX()].getClass()==Pancarte.class){
-                messages.afficher();}//si c'est pancarte, un message d'affiche
-            else if (map[Adlez.getY()][Adlez.getX()].getClass()==Teleporteur.class){//si c'est téléporteur
-                boite.add(Adlez.getX());boite.add(Adlez.getY());//stocke les données x y de Adlez actuellement
-                Adlez.setY(map[boite.get(1)][boite.get(0)].getTpy());
-                //change le Y de Adlez (avec les données du téléporteur ou Adlez était).
-                //Mais vu que après setY les coordonnées de Adlez change, utiliser getY et getX ne va pas marcher
-                Adlez.setX(map[boite.get(1)][boite.get(0)].getTpx());
-                map[Adlez.getY()][Adlez.getX()].setHero();
-                //aux coordonnées de la classe téléporteur, afficher &
-                map[boite.get(1)][boite.get(0)].revenir();
-                //et effacer l'endroit ou Adlez était avant.
-                boite.clear();//vide la boite pour la suite.
-            }
-        }
-        else if (map[Adlez.getY()][Adlez.getX()+1].getClass()!=Mur.class&&map[Adlez.getY()][Adlez.getX()+1].getClass()!=Plancher.class){
-            //même chose, à gauche
-            if (map[Adlez.getY()][Adlez.getX()+1].getClass()==Pancarte.class){
-                messages.afficher();}
-            else if (map[Adlez.getY()][Adlez.getX()+1].getClass()==Teleporteur.class){
-                boite.add(Adlez.getX());boite.add(Adlez.getY());
-                Adlez.setY(map[boite.get(1)][boite.get(0)+1].getTpy());
-                Adlez.setX(map[boite.get(1)][boite.get(0)+1].getTpx());
-                map[Adlez.getY()][Adlez.getX()].setHero();
-                map[boite.get(1)][boite.get(0)].revenir();
-                boite.clear();
-
-            }else if (map[Adlez.getY()][Adlez.getX()+1].getClass()==Tresor.class){
-                //vu que Adlez ne peut pas marcher sur un trésor, il n'est pas au premier if
-                map[Adlez.getY()][Adlez.getX()+1].setItem(niveau,Adlez.getX()+1,Adlez.getY());
-                //lit le fichier pour voir c'est quoi l'item de cette coordonée.
-                map[Adlez.getY()][Adlez.getX()+1].utiliser(Adlez);
-                //l'utilise sur Adlez
-            }
-        }else if (map[Adlez.getY()][Adlez.getX()-1].getClass()!=Mur.class&&map[Adlez.getY()][Adlez.getX()-1].getClass()!=Plancher.class){
-            if (map[Adlez.getY()][Adlez.getX()-1].getClass()==Pancarte.class){
-                messages.afficher();}
-            else if (map[Adlez.getY()][Adlez.getX()-1].getClass()==Teleporteur.class){
-                boite.add(Adlez.getX());boite.add(Adlez.getY());
-                Adlez.setY(map[boite.get(1)][boite.get(0)-1].getTpy());
-                Adlez.setX(map[boite.get(1)][boite.get(0)-1].getTpx());
-                map[Adlez.getY()][Adlez.getX()].setHero();
-                map[boite.get(1)][boite.get(0)].revenir();
-                boite.clear();
-            }else if (map[Adlez.getY()][Adlez.getX()-1].getClass()==Tresor.class){
-                map[Adlez.getY()][Adlez.getX()-1].setItem(niveau,Adlez.getX()-1,Adlez.getY());
-                map[Adlez.getY()][Adlez.getX()-1].utiliser(Adlez);
-            }
-        }else if (map[Adlez.getY()+1][Adlez.getX()].getClass()!=Mur.class&&map[Adlez.getY()+1][Adlez.getX()].getClass()!=Plancher.class){
-            if (map[Adlez.getY()+1][Adlez.getX()].getClass()==Pancarte.class){
-                messages.afficher();}
-            else if (map[Adlez.getY()+1][Adlez.getX()].getClass()==Teleporteur.class){
-                boite.add(Adlez.getX());boite.add(Adlez.getY());
-                Adlez.setY(map[boite.get(1)+1][boite.get(0)].getTpy());
-                Adlez.setX(map[boite.get(1)+1][boite.get(0)].getTpx());
-                map[Adlez.getY()][Adlez.getX()].setHero();
-                map[boite.get(1)][boite.get(0)].revenir();
-                boite.clear();
-            }else if (map[Adlez.getY()+1][Adlez.getX()].getClass()==Tresor.class){
-                map[Adlez.getY()+1][Adlez.getX()].setItem(niveau,Adlez.getX(),Adlez.getY()+1);
-                map[Adlez.getY()+1][Adlez.getX()].utiliser(Adlez);
-            }
-        }else if (map[Adlez.getY()-1][Adlez.getX()].getClass()!=Mur.class&&map[Adlez.getY()-1][Adlez.getX()].getClass()!=Plancher.class){
-            if (map[Adlez.getY()-1][Adlez.getX()].getClass()==Pancarte.class){
-                messages.afficher();}
-            else if (map[Adlez.getY()-1][Adlez.getX()].getClass()==Teleporteur.class){
-                boite.add(Adlez.getX());boite.add(Adlez.getY());
-                Adlez.setY(map[boite.get(1)-1][boite.get(0)].getTpy());
-                Adlez.setX(map[boite.get(1)-1][boite.get(0)].getTpx());
-                map[Adlez.getY()][Adlez.getX()].setHero();
-                map[boite.get(1)][boite.get(0)].revenir();
-                boite.clear();
-            }else if (map[Adlez.getY()-1][Adlez.getX()].getClass()==Tresor.class){
-                map[Adlez.getY()-1][Adlez.getX()].setItem(niveau,Adlez.getX(),Adlez.getY()-1);
-                map[Adlez.getY()-1][Adlez.getX()].utiliser(Adlez);
-            }
-        }
-
     }
 
 }
